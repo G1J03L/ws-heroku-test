b9cb54bb8d004972ef9ecc19e20cb16ef5de2999
Joel Gallant <joel.i.gallant@gmail.com>
2015-05-26 02:53:55 -0300

    Minor changes.

--- app/views/sysJs.scala.js
+++ app/views/sysJs.scala.js
@@ -1,9 +1,10 @@
 $(function() {
      var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket;
      var sysSocket = new WS("@routes.Application.pingWs().webSocketURL(request, true)");
-     
+     var incr = 0; 
+    
      var sysEvent = function (event) {
-          $("sysMsg").html("Wait for it... " + event.data);
+          $("sysMsg").html("Wait for it... " + event.data + "\t" + incr++);
      }
      
      sysSocket.onMessage = sysEvent;
